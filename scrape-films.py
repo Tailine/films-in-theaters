64ffed44b38dd58856bab6445f2cf4c901d439b0
Tailine Chagas <tailinechagas@gmail.com>
2019-12-28 15:08:26 -0300

    feat: add auditorium

--- scrape-films.py
+++ scrape-films.py
@@ -5,9 +5,6 @@ separator = "===============================================================\n"
 req = requests.get('https://www.cinemark.com.br/salvador/cinemas')
 file = open("movies.txt", "w")
 
-def display_results(title, times):
-  print("FILME: ", title, "\nHORÁRIOS: ", times, "\n")
-
 def main():
   if req.status_code == 200:
     page_content = req.content
@@ -15,18 +12,19 @@ def main():
     films_data = soup.select(".tabs-content .active .theater")
     for data in films_data:
       title = data.select('h3 a')[0].text # get film title
-      file.write(title) 
+      buy_ticket_link = (data.select('.actions .btn.btn-primary.btn-buy.reversed')[0]).get('href')
+      file.write(f"{title}\n\nComprar ingressos: {buy_ticket_link}\n\n") 
 
-      theater_times = data.select(".theater-times")
-      for info in theater_times:
-        film_times = data.select(".times-options")
-        film_languages = list(map(lambda span: span.text ,data.select(".times-labels.times-labels-right li span")))
-        
-        for i, time in enumerate(film_times):
-          time_option = ", ".join(map(lambda span: span.text , time.select("li span")))
-          file_content = f'\nHorários: {time_option} \nIdiomas: {film_languages[i]}\n\n'
-          file.write(file_content)
-        file.write(separator)
+      auditorium = data.find("span", { "class": "times-auditorium" }).text
+      file.write(auditorium)
+
+      film_times = data.select(".times-options")
+      film_languages = list(map(lambda span: span.text ,data.select(".times-labels.times-labels-right li span")))
+      
+      for i, time in enumerate(film_times):
+        time_option = ", ".join(map(lambda span: span.text , time.select("li span")))
+        file.write(f'\nHorários: {time_option} \nIdiomas: {film_languages[i]}\n\n')
+      file.write(separator)
     # TODO - add link
     # TODO - write results into file 
   file.close()
