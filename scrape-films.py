14e9d4c11893834af2b9a15d011ff3fb95bf8779
Tailine Chagas <tailinechagas@gmail.com>
2019-12-28 00:25:26 -0300

    feat: retrieve title and times of films

--- scrape-films.py
+++ scrape-films.py
@@ -3,9 +3,20 @@ import requests
 
 req = requests.get('https://www.cinemark.com.br/salvador/cinemas')
 
+def display_results(title, times):
+  print("FILME: ", title, "\nHORÁRIOS: ", times, "\n")
+
 if req.status_code == 200:
   page_content = req.content
   soup = BeautifulSoup(page_content, 'html.parser')
-  films_title = soup.select(".tabs-content .active .theater .theater-header h3")
-  for film_title in films_title:
-    print(film_title.text)
+  films_data = soup.select(".tabs-content .active .theater")
+  for data in films_data:
+    # get film title
+    title = data.select('h3 a')[0].text
+    # get film times
+    film_times = data.select(".times-options li span")
+    times = ", ".join(list(map(lambda html: html.text, film_times)))
+    
+    # TODO - add link
+    # TODO - write results into file 
+    display_results(title, times)
