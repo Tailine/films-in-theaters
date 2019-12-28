133c76726f456179bb765abaf0908af744c0b2d6
Tailine Chagas <tailinechagas@gmail.com>
2019-12-28 14:28:10 -0300

    write results into file

--- scrape-films.py
+++ scrape-films.py
@@ -1,31 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 
+separator = "===============================================================\n"
 req = requests.get('https://www.cinemark.com.br/salvador/cinemas')
+file = open("movies.txt", "w")
 
 def display_results(title, times):
   print("FILME: ", title, "\nHORÁRIOS: ", times, "\n")
 
-if req.status_code == 200:
-  page_content = req.content
-  soup = BeautifulSoup(page_content, 'html.parser')
-  films_data = soup.select(".tabs-content .active .theater")
-  for data in films_data:
-    # get film title
-    title = data.select('h3 a')[0].text
-    # get film times
-    print(title)
-    theater_times = data.select(".theater-times")
-    for info in theater_times:
-      film_times = data.select(".times-options")
-      film_languages = list(map(lambda span: span.text ,data.select(".times-labels.times-labels-right li span")))
-      
-      for i, time in enumerate(film_times):
-        time_option = ", ".join(map(lambda span: span.text , time.select("li span")))
-        print(time_option)
-        print(film_languages[i], "\n")
-      print("===============================================================")
+def main():
+  if req.status_code == 200:
+    page_content = req.content
+    soup = BeautifulSoup(page_content, 'html.parser')
+    films_data = soup.select(".tabs-content .active .theater")
+    for data in films_data:
+      title = data.select('h3 a')[0].text # get film title
+      file.write(title) 
 
+      theater_times = data.select(".theater-times")
+      for info in theater_times:
+        film_times = data.select(".times-options")
+        film_languages = list(map(lambda span: span.text ,data.select(".times-labels.times-labels-right li span")))
+        
+        for i, time in enumerate(film_times):
+          time_option = ", ".join(map(lambda span: span.text , time.select("li span")))
+          file_content = f'\nHorários: {time_option} \nIdiomas: {film_languages[i]}\n\n'
+          file.write(file_content)
+        file.write(separator)
     # TODO - add link
     # TODO - write results into file 
-    # display_results(title, times)
+  file.close()
+
+main()
